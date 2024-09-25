3c4d54ef7469aa8c4596bee59d7d0651f430c7a1
willyam ribiere <willyamrib@gmail.com>
2024-09-25 14:54:57 +0200

    Correction database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,15 @@
 // path: ./config/env/production/database.ts
 
-import { parse } from "pg-connection-string";
-const config = parse(process.env.DATABASE_URL);
-
 export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: "clhtb6lu92mj2.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com", // Host de la base de données
+      port: 5432, // Port utilisé par PostgreSQL
+      database: "dct63fh5nej8jp", // Nom de la base de données
+      user: "ueibs0ann1qbuu", // Utilisateur de la base de données
+      password:
+        "pc917933459e298636b4537d128d5c2d4b5f6e8c994ffaa5758e680ff9d2f3b95", // Mot de passe de l'utilisateur
       ssl: {
         rejectUnauthorized: false,
       },
